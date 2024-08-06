231e9410d875b6f400adf3b98bc242c846ce7af6
hym <ghrltjdtprbs@gmail.com>
2024-08-07 08:43:24 +0900

    [feat] 테스트용 프로젝트 생성
    변경 사항 :
     - 프로젝트 생성
     - 테스트용 get api 생성
       GET /hello

---
+++ src/test/java/com/no_downtime/NoDowntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no_downtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoDowntimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
